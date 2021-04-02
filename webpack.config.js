eeb3a58a364ee6825e78d8686f7c67bb9fcf5fb8
litianhua <18702971504@163.com>
2021-04-02 17:08:54 +0800

    将分离后的css打包到指定路径中

--- webpack.config.js
+++ webpack.config.js
@@ -81,8 +81,8 @@ module.exports = {
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: '[name].css',
-      chunkFilename: '[id].css',
+      filename: 'css/[name].css',
+      chunkFilename: 'css/[id].css',
     }),
   ]
 }

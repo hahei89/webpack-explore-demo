7a38082cf7f0f97927bf364168d707c7783cf07f
litianhua <18702971504@163.com>
2021-04-02 16:47:25 +0800

    修改内容信息

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render () {
     return (
       <div className='render'>
-        <h1>Hello, world! this  sfdsfsdf is here</h1>
+        <h1>Hello, world! this is here</h1>
       </div>
     )
   }
